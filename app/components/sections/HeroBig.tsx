40f119cff79d115e57d9f025911bed219632a37a
TheLore85 <garofalo.digital@gmail.com>
2025-01-30 12:08:06 +0100

    feat - form: newsletter

--- app/components/sections/HeroBig.tsx
+++ app/components/sections/HeroBig.tsx
@@ -1,5 +1,6 @@
 import Image from 'next/image'
 import CtaMain from '../buttons/CtaMain'
+import Newsletter from '../form/Newsletter'
 
 type Props = {}
 
@@ -12,8 +13,10 @@ export default function HeroBig({}: Props) {
             <div>
               <h1 className="font-bold mb-10 text-primary-foreground">¿Renovar tu seguro de moto y coche con un 25% de ahorro? ¡Hecho!</h1>
               <p className="text-3xl text-primary-foreground mb-10">Mejora de precio o condiciones, con un seguro 100% personalizado</p>
-              <CtaMain />
+              {/* <CtaMain /> */}
+              <Newsletter />
             </div>
+
             <Image src="/img/components/hero/hero_img.png" alt="hero image" className="w-[500px] h-[500px] object-contain hidden lg:block" width={500} height={500} />
           </div>
         </div>
