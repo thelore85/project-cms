c434cdd9723828e4c9b4b808eeb84b48fe1fb35f
TheLore85 <garofalo.digital@gmail.com>
2025-02-04 02:29:46 +0100

    fix - routing: footer linkg

--- app/components/sections/retail/FooterRetail.tsx
+++ app/components/sections/retail/FooterRetail.tsx
@@ -13,21 +13,21 @@ export default function FooterRetail({}: Props) {
         {/* ROW 1 */}
         <div className="md:flex justify-between gap-10 mb-24">
           <div className="p-10  w-1/3">
-            <Link href="/" className="block text-4xl font-bold">
+            <Link href="/es" className="block text-4xl font-bold">
               Zeeguros
             </Link>
           </div>
           <div className="p-10 w-1/3">
             <p className="font-bold text-xl mb-10">Contacts</p>
-            <Link href="/" className="block mb-5">
+            <Link href="/es" className="block mb-5">
               <FontAwesomeIcon icon={faEnvelope} className="w-5 h-5 inline-block me-3" />
               info@zeeguros.com
             </Link>
-            <Link href="/" className="block mb-5">
+            <Link href="/es" className="block mb-5">
               <FontAwesomeIcon icon={faEnvelope} className="w-5 h-5 inline-block me-3" />
               workwithus@zeeguros.com
             </Link>
-            <Link href="/" className="block mb-5">
+            <Link href="/es" className="block mb-5">
               <FontAwesomeIcon icon={faEnvelope} className="w-5 h-5 inline-block me-3" />
               marketing@zeeguros.com
             </Link>
@@ -38,13 +38,13 @@ export default function FooterRetail({}: Props) {
           </div>
           <div className="p-10 w-1/3">
             <p className="font-bold text-xl mb-10">Pages</p>
-            <Link href="/" className="block mb-5">
+            <Link href="/es" className="block mb-5">
               Homepage
             </Link>
-            <Link href="/legal" className="block mb-5">
+            <Link href="/es/legal" className="block mb-5">
               Terms and Conditions
             </Link>
-            <Link href="/blog" className="block mb-5">
+            <Link href="/es/blog" className="block mb-5">
               Blog
             </Link>
             <Link href="/admin" className="block mb-5">
