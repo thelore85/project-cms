fe25618316a77d0df76897cc169a8e285d862749
TheLore85 <garofalo.digital@gmail.com>
2025-02-05 12:46:50 +0100

    fix - routing: using lang params v4

--- app/(mainHome)/layout.tsx
+++ app/(mainHome)/layout.tsx
@@ -1,19 +1,17 @@
 'use client'
 
 import Footer from '@/app/components/sections/retail/FooterRetail'
-import ScrollToTop from '@/app/components/utility/ScrollToTop'
 import NavigatorRetail from '@/app/components/navigations/retail/NavigatorRetail'
-import useStore from '@/store/store'
 
 type Props = {
   children: React.ReactNode
-  props: {
+  params: {
     lang: string
   }
 }
 
-export default function BrokerLayout({children, props}: Props) {
-  const {lang} = useStore()
+export default function BrokerLayout({children, params}: Props) {
+  const {lang} = params
 
   return (
     <>
