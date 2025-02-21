93b22ed9ae47799abd6f1d44cef099ef45219c8e
TheLore85 <garofalo.digital@gmail.com>
2025-02-21 09:36:55 +0100

    fix - debugging  pageSlug/layout - adding promise in type

--- app/[lang]/[pageSlug]/layout.tsx
+++ app/[lang]/[pageSlug]/layout.tsx
@@ -2,20 +2,28 @@ import LayoutTemplate from './LayoutTemplate'
 
 type Props = {
   children: React.ReactNode
-  params: {
-    lang: string
-    pageSlug: string
-  }
+  params:
+    | {
+        lang: string
+        pageSlug: string
+      }
+    | Promise<{
+        lang: string
+        pageSlug: string
+      }>
 }
 
 export default async function PageLayout({children, params}: Props) {
+  // Ensure params is resolved properly, await if it's a Promise
+  const resolvedParams = await (params instanceof Promise ? params : Promise.resolve(params))
+
   return (
     <>
       {/* ////////////////// */}
       {/* Add metadata here  */}
       {/* ////////////////// */}
 
-      <LayoutTemplate params={params}>{children}</LayoutTemplate>
+      <LayoutTemplate params={resolvedParams}>{children}</LayoutTemplate>
     </>
   )
 }
